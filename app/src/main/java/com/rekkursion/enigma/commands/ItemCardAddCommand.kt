efd23076bfe32a0e5c226fe170584efce0724273
Rekkursion <rekkursion233@gmail.com>
2020-02-15 15:40:09 +0800

    Set focus on the newest card when creating new items

--- app/src/main/java/com/rekkursion/enigma/commands/ItemCardAddCommand.kt
+++ app/src/main/java/com/rekkursion/enigma/commands/ItemCardAddCommand.kt
@@ -1,6 +1,7 @@
 package com.rekkursion.enigma.commands
 
 import android.annotation.SuppressLint
+import android.widget.EditText
 import com.rekkursion.enigma.activities.NewItemActivity
 import com.rekkursion.enigma.enums.ItemType
 import com.rekkursion.enigma.listeners.OnActionsClickListener
@@ -40,5 +41,8 @@ class ItemCardAddCommand(newItemActivity: NewItemActivity): ItemCardCommand {
         // add this card and enable the submit button
         mNewItemActivityInstance.mLlyCardsContainer.addView(card)
         mNewItemActivityInstance.mCancelSubmitButtonBar.setEnabilities(isBtnSubmitEnabled = true)
+
+        // focus on the newest card's first edit-text
+        card.getAllFieldsContentViews().filter { it is EditText }.getOrNull(0)?.requestFocus()
     }
 }
